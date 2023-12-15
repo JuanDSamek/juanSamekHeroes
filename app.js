092d5420847f4e38b9fca014e43fd6bbabd10550
JuanDSamek <juanelpato04@gmail.com>
2023-12-15 11:44:24 -0300

    se agregan los archivos al repositorio

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require("path");
+
+
+const app = express();
+const port = 3030;
+
+// FUNCIONES
+const createPath = (filename) => path.join(__dirname,`./view/${filename}.html`)     // esta funcion trae la reuta para hacerlo dinamico
+ 
+
+//  CONFIGURACIONES
+app.use(express.static("public"));
+
+app.get("/",(req,res) => {
+    res.sendFile(createPath("index"))
+})
+ 
+app.get("/babbage",(req,res) => {
+    res.sendFile(createPath("babbage"))
+})
+
+app.get("/berners-lee",(req,res) => {
+    res.sendFile(createPath("bberners-lee"))
+})
+
+app.get("/clarke",(req,res) => {
+    res.sendFile(createPath("/clarke"))
+})
+
+app.get("/hamilton",(req,res) => {
+    res.sendFile(createPath("/hamilton"))
+})
+
+app.get("/hopper",(req,res) => {
+    res.sendFile(createPath("/hopper"))
+})
+
+app.get("/lovelace",(req,res) => {
+    res.sendFile(createPath("/lovelace"))
+})
+
+app.get("/berners-lee",(req,res) => {
+    res.sendFile(createPath("bberners-lee"))
+})
+
+
+app.listen(port, () => console.log(`http://localhost:${port}`)) 
